b9c66e0b4d917f4407c2e329fd1afec6da5c5493
sourabh singh <saurabh.th45@gmail.com>
2025-02-25 11:30:47 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+      int x = 5;
+      int y = 3;
+      int multiply = x * y;
+      System.out.println(multiply); 
+    }
+  }
+  
