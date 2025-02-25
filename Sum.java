af04265cc0c2db676aa332f20efc8df635062a2f
sourabh singh <saurabh.th45@gmail.com>
2025-02-25 09:45:33 +0530

    Added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args) {
       int x = 5;
-      int y = 6;
+      int y = 3;
       int sum = x + y;
       System.out.println(sum); // Print the sum of x + y
     }
